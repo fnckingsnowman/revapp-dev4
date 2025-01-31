2f5d91e1e5e3132515c9b9b0dc0462076066d4dd
fnckingsnowman <nedzhong@hotmail.com>
2025-02-01 01:03:31 +0800

    changes: - properly implemented sidenavbarpanething - added pages for discover and bleconnect - added the device dropdownslot in the header

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,32 +1,50 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI.Xaml;
+using System;
+using System.Windows;
 
 namespace RevoluteConfigApp
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            nvSample.ItemInvoked += NvSample_ItemInvoked;
         }
 
+        private void NvSample_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                // Handle settings item invocation if needed
+            }
+            else
+            {
+                var selectedItem = args.InvokedItemContainer as NavigationViewItem;
+                if (selectedItem != null)
+                {
+                    string pageTag = selectedItem.Tag.ToString();
+                    Type pageType = null;
+
+                    switch (pageTag)
+                    {
+                        case "Discover":
+                            pageType = typeof(Pages.DiscoverPage);
+                            break;
+                        case "BLEPage":
+                            pageType = typeof(Pages.BLEPage);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (pageType != null)
+                    {
+                        contentFrame.Navigate(pageType);
+                    }
+                }
+            }
+        }
     }
 }
