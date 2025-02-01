e8ba2423ad7f5c79433bbf177f5bf72c3421a976
fnckingsnowman <nedzhong@hotmail.com>
2025-02-01 23:08:15 +0800

    fucking hell

---
+++ Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace RevoluteConfigApp.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            // Convert bool to Visibility
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            // Convert Visibility back to bool
+            if (value is Visibility visibility)
+            {
+                return visibility == Visibility.Visible;
+            }
+            return false;
+        }
+    }
+}
---
+++ Converters/InverseBoolToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace RevoluteConfigApp.Converters
+{
+    public class InverseBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            // Convert bool to Visibility (inverse logic)
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            // Convert Visibility back to bool (inverse logic)
+            if (value is Visibility visibility)
+            {
+                return visibility != Visibility.Visible;
+            }
+            return true;
+        }
+    }
+}
